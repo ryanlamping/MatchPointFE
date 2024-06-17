d6f7f1fb802a8ca89f4fda7ec41f25202b58c04f
Ryan Lamping <ryan.m.lamping@gmail.com>
2024-06-16 20:27:33 -0500

    Initial Commit

---
+++ MatchPoint/MatchPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchPointApp.swift
+//  MatchPoint
+//
+//  Created by Ryan Lamping on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MatchPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
